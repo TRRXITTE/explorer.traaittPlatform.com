589d97ec51a7ca9ea4aff9e96ee894d9a75a36ef
traaitt | TRRXITTE inc <traaitt@icloud.com>
2020-08-14 11:16:31 +0200

    Initial commit

---
+++ api/config.php
@@ -0,0 +1,5 @@
+<?php
+
+// used for API calls change it to your own Qwertycoin RPC node
+// daemon:port 8197 is the default RPC Port
+$apiNode = 'https://asia-southeast.traaitt.com';
---
+++ config.js
@@ -0,0 +1,24 @@
+var api = 'https://explorer.traaittplatform.com';
+var api_blockexplorer = 'https://asia-southeast.traaitt.com';
+var apiList = [
+	'https://asia-southeast.traaitt.com'
+];
+var blockTargetInterval = 644;
+var coinUnits = 100000000;
+var symbol = 'ETRX';
+var refreshDelay = 15000;
+// pools stats by MainCoins
+var poolsStat =
+	[
+		["traaittPlatform offical","https://traaittsupply.cash:8117/stats"]
+    ];
+var nodesStat =
+	[
+		['http://us-east.traaitt.com:24496'],
+		['http://us-west.traaitt.com:24496'],
+		['http://asia-southeast.traaitt.com:24496'],
+		['http://asia-east.traaittnode.com:24496'],
+		['http://asia-north.traaittnode.com:24496'],
+		['http://europe-west.traaittnode.com:24496'],
+		['https://europe-east.traaittnode.com:24496']
+    ];
