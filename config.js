32f8cc8e3a5e2f767cc6a55c5f5bb0c8245fd352
traaitt | TRRXITTE inc <traaitt@icloud.com>
2020-12-22 14:17:58 +0100

    update Information

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = 'https://explorer.traaittplatform.com';
-var api_blockexplorer = 'https://asia-southeast.traaitt.com';
+var api_blockexplorer = 'https://us-west.traaitt.com';
 var apiList = [
-	'https://asia-southeast.traaitt.com'
+	'https://us-west.traaitt.com'
 ];
 var blockTargetInterval = 644;
 var coinUnits = 100000000;
@@ -10,15 +10,10 @@ var refreshDelay = 15000;
 // pools stats by MainCoins
 var poolsStat =
 	[
-		["traaittPlatform offical","https://traaittsupply.cash:8117/stats"]
+		["TRRXITTE Inc., ETRX Main","https://traaittsupply.cash:8117/stats"]
     ];
 var nodesStat =
 	[
-		['http://us-east.traaitt.com:24496'],
 		['http://us-west.traaitt.com:24496'],
-		['http://asia-southeast.traaitt.com:24496'],
-		['http://asia-east.traaitt.com:24496'],
-		['http://asia-north.traaitt.com:24496'],
-		['http://europe-west.traaitt.com:24496'],
-		['https://europe-east.traaitt.com:24496']
+		['http://europe-west.traaitt.com:24496']
     ];
